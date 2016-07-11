41ecc8bd9683678cee2d250c8e8a3420674da530
Zachary R <zachary.mt.ranes@gmail.com>
2016-07-10 22:11:11 -0400

    Went back to saving points in text file, other small changes

--- HousePoints_bot.py
+++ HousePoints_bot.py
@@ -1,6 +1,5 @@
 #Written by Zachary Ranes
 
-import pickle
 import configparser
 import telebot
 from telebot import types
@@ -9,32 +8,40 @@ from telebot import types
 config = configparser.ConfigParser()
 config.read("HousePoints_bot_config.cfg")
 
-#associate bot with token
+#associate bot with token 
 bot = telebot.TeleBot(config['telegram_bot_api']['telegram_token'])
 
 #array for holding house points
 #Gryffindor, Slytherin, Ravenclaw, Hufflepuff
-bot.HousePoints = [0,0,0,0]
+HousePoints = [0,0,0,0]
 
 #reading in records from file when bot starts up
 try:
-	with open("HousePoints_bot_record.pickle","rb") as record:
-		bot.HousePoints = pickle.load(record)
+	with open("HousePoints_bot_record.txt","r") as record:
+		HousePoints = [int(score) for score in record.read().split(' ')]
 except:
-	print("No HousePoints_bot_record.pickle file found")
+	print("No HousePoints_bot_record.text file found, new one will be made apone first points awarded")
 
 # list of prefects telegram id are in the config file
 prefects = [int(id) for id in config.get("prefects", "id").split(", ")]
-userStep = {}
-for id in prefects:
-	userStep[id] = 4
+
 #Step 4 is rest becoues 0-3 are the four houses indexs wich makes it easyest to have those as the other steps
+userStep = {} 
+for id in prefects: userStep[id] = 4
+
 
 #hiddin comand to check id to be added to prefect list
 @bot.message_handler(commands=['prefect_test'])
 def CheckID(message):
 	bot.send_message(message.chat.id, "Here is the ID to be added to the prefect list " + str(message.from_user.id))
+		
+		
+#Command for anyone to use to see how many points each house has
+@bot.message_handler(commands=['house_totals'])
+def PointsCount(message):
+    bot.send_message(message.chat.id, "Gryffindor:  " + str(HousePoints[0]) + "\nSlytherin:  " + str(HousePoints[1]) + "\nRavenclaw:  " + str(HousePoints[2]) + "\nHufflepuff:  " + str(HousePoints[3]))
 
+	
 #so names of houses can be called by their index
 def HouseNames(houseIdex):
 	if houseIdex == 0:
@@ -46,27 +53,17 @@ def HouseNames(houseIdex):
 	if houseIdex == 3:
 		return "Hufflepuff"
 	
-#Command for anyone to use to see how many points each house has
-@bot.message_handler(commands=['house_totals'])
-def PointsCount(message):
-    bot.send_message(message.chat.id, "Gryffindor:  " + str(bot.HousePoints[0]) +
-									  "\nSlytherin:  " + str(bot.HousePoints[1]) +
-									  "\nRavenclaw:  " + str(bot.HousePoints[2]) +
-									  "\nHufflepuff:  " + str(bot.HousePoints[3]))
-
-
-#The force reply for the next four commands
-adding = types.ForceReply(selective=True)
-
+	
 #is numbers 2 becouse it is called from 1G, 1S, 1R, 1H
 def Addpoints2(index, message):
+	howMany = types.ForceReply(selective=True)
 	if message.from_user.id in prefects:
-		bot.reply_to(message, "How many points do you wish to award to " + str(HouseNames(index)) + "?", reply_markup=adding)
+		bot.reply_to(message, "How many points do you wish to award to " + str(HouseNames(index)) + "?", reply_markup=howMany)
 		userStep[message.from_user.id] = index
 	else:
 		bot.send_message(message.chat.id, "Only prefects can award points")
-
-
+	
+	
 #the four main comands of the bot
 @bot.message_handler(commands=['points_gryffindor'])
 def AddPoints1G(message):
@@ -84,14 +81,15 @@ def AddPoints1R(message):
 def AddPoints1H(message):
 	Addpoints2(3, message)
 
+	
 #this handles the responce to the four comands
-@bot.message_handler(func=lambda message: message.from_user.id in prefects and userStep[message.from_user.id] != 4)
+@bot.message_handler(func=lambda message: userStep[message.from_user.id] != 4 and message.from_user.id in prefects)
 def AddPoints3(message):
 	try:
 		index = userStep[message.from_user.id]
-		bot.HousePoints[index]+= int(message.text)
-		with open("HousePoints_bot_record.pickle","wb") as newRecord:
-			pickle.dump(bot.HousePoints, newRecord)
+		HousePoints[index]+= int(message.text)
+		with open("HousePoints_bot_record.txt","w") as newRecord:
+			newRecord.write(" ".join([str(score) for score in HousePoints]))
 		bot.send_message(message.chat.id, message.text + " Points awarded to " + str(HouseNames(index)))
 
 	except:
@@ -99,4 +97,5 @@ def AddPoints3(message):
 
 	userStep[message.from_user.id] = 4
 
+	
 bot.polling()
