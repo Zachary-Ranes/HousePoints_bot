d05f95edc07b736fc9320320cfff95fdf5f3f96f
Zachary-Ranes <zachary.mt.ranes@gmail.com>
2016-11-02 15:18:42 -0400

    huge rework, chats have their own schools, still need to finish setting

--- HousePoints_bot.py
+++ HousePoints_bot.py
@@ -1,162 +1,323 @@
-#Written by Zachary Ranes
-#Writen for Python 3.4
+#Author: Zachary Ranes
+#Written in Python 2.7, requires eternnoir/pyTelegramBotAPI to run
 
-import configparser
-import pickle
-import datetime 
-import time
+import ConfigParser
+import cPickle as pickle
+
 import telebot
 from telebot import types
 
-#read config file
-config = configparser.ConfigParser()
-config.read("HousePoints_bot_config.cfg")
+from HousePoints_bot_Scores import School
 
-#associate bot with token 
+#Read configure file
+config = ConfigParser.ConfigParser()
+config.read("Config.cfg")
+
+#Associates bot with token 
 bot = telebot.TeleBot(config.get("telegram_bot_api","telegram_token"))
 
-#array for holding house points
-#Gryffindor, Slytherin, Ravenclaw, Hufflepuff
-HousePoints = [0,0,0,0]
+#This dictionary holds an object for each chat (chat being a school) 
+schools = {}
 
-#reading in records from file when bot starts up
+#key is a messages ID returns a users ID
+messages_awaiting_responses = {}
+
+#Loads scores from file when bots startup
 try:
-	with open("HousePoints_bot_record.pickle","rb") as record:
-		HousePoints = pickle.load(record)
-	record.close()
-except:
-	print("No HousePoints_bot_record.pickle file found, new one will be made apone first points awarded")
+    schools = pickle.load(open("HousePoints_bot_Data.p", "rb"))
+#The first time this bot runs this will fail so this bit is in a try
+except: pass
 
+#The first person in a chat that runs this command becomes headmaster
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    key = message.chat.id
+    if key in schools:
+        bot.reply_to(message, "There is already a school established here.")
+    if message.chat.type == "private":
+        bot.reply_to(message, "You can't start a school in a private chat.")
+    if key not in schools and message.chat.type == "group"\
+    or key not in schools and message.chat.type == "supergroup":
+        schools[key] = School(message.from_user.id,
+                              message.from_user.first_name,
+                              message.from_user.last_name)
+        pickle.dump( schools, open( "HousePoints_bot_Data.p", "wb" ) )
+        bot.reply_to(message, "A new school has been formed in this chat "\
+                              "with you as its headmaster.")
+        
+#Expected command for bots
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    bot.reply_to(message, "When someone runs the /start command in a chat for "\
+                          "the first time a school if formed in that chat and "\
+                          "the user that runs the command becomes the "\
+                          "headmaster of the school. The headmaster can add "\
+                          "a chat member as a prefect after they run the "\
+                          "/prefect_test command with the /add_prefect command"\
+                          ". The Headmaster can also remove prefects, add "\
+                          "houses to the school and remove house from the "\
+                          "school with the /school_settings command. The "\
+                          "headmaster and prefect can award points to the "\
+                          "houses with the /award_points command. Any member "\
+                          "of the chat can run the /house_totals command "\
+                          "to see the houses of the school and their points")
 
-#an array that holds last months scores and the month number from which those scores are from
-LastMonthsPoints = [0,0,0,0,0]
-
-#reading in records of last months points
-try:
-	with open("HousePoints_bot_record_lastMonth.pickle","rb") as oldRecord:
-		LastMonthsPoints = pickle.load(oldRecord)
-	oldRecord.close()
-except:
-	print("No HousePoints_bot_record_lastMonth.pickle file found")
-
-	
-# list of prefects telegram id are in the config file
-prefects = [int(id) for id in config.get("prefects", "id").split(", ")]
-
-#Step 4 is rest becoues 0-3 are the four houses indexs wich makes it easyest to have those as the other steps
-userStep = {} 
-for id in prefects: userStep[id] = 4
-
-
-#Fuction that check to see if new month has turned yet
-def LastMonthUpdate(message):
-	if int(datetime.datetime.now().month) != LastMonthsPoints[4]:
-		LastMonthsPoints[0] = HousePoints[0]
-		LastMonthsPoints[1] = HousePoints[1]
-		LastMonthsPoints[2] = HousePoints[2]
-		LastMonthsPoints[3] = HousePoints[3]
-		LastMonthsPoints[4] = int(datetime.datetime.now().month)
-		HousePoints[0] = 0
-		HousePoints[1] = 0
-		HousePoints[2] = 0
-		HousePoints[3] = 0
-		with open("HousePoints_bot_record_lastMonth.pickle","wb") as newOldRecord:
-			pickle.dump(LastMonthsPoints, newOldRecord)
-		newOldRecord.close()
-
-	
-	
-
-#hiddin comand to check id to be added to prefect list
+#Prefect logic will add the play to a list that the headmaster can approve
 @bot.message_handler(commands=['prefect_test'])
-def CheckID(message):
-	bot.send_message(message.chat.id, "Here is the ID to be added to the prefect list " + str(message.from_user.id))
-		
-		
-#Command for anyone to use to see how many points each house has
+def command_prefect_test(message):
+    key = message.chat.id 
+    if key in schools:
+        output = schools[key].add_to_prefect_waiting_list(message.from_user.id,
+                                                   message.from_user.first_name,
+                                                   message.from_user.last_name)
+        bot.reply_to(message, output)
+    else:
+        bot.reply_to(message, "There is not school in the chat.")
+
+#Allows the headmaster to add player that have rung prefect test to the prefects
+@bot.message_handler(commands=['add_prefect'])
+def command_add_prefect(message):
+    key = message.chat.id
+    if key in schools:
+        output = schools[key].review_prefect_waiting_list(message.from_user.id)
+        bot.reply_to(message, output[0], reply_markup=output[1])
+    else:
+        bot.reply_to(message, "There is not school in the chat.")
+
+#Callback for the adding of prefect from a list that is given to headmaster
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                                                and call.data[0:24] == \
+                                                "HousePoints_add_prefect_")
+def callback_from_add_prefect(call):
+    key = call.message.chat.id 
+    output = schools[key].add_prefect(call.message.from_user.id,
+                                      int(call.data[27:]))
+    if output:
+        pickle.dump( schools, open( "HousePoints_bot_Data.p", "wb" ) )
+        try:
+            bot.edit_message_text(output, 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id)
+        except: pass
+
+#Responds with a list of house that the headmaster or prefect can give points to 
+@bot.message_handler(commands=['award_points'])
+def command_select_houese_to_award_points(message):
+    key = message.chat.id
+    if key in schools:
+        output = schools[key].houses_to_award_points(message.from_user.id)
+        bot.reply_to(message, output[0], reply_markup=output[1])
+    else:
+        bot.reply_to(message, "There is not school in the chat.")
+
+#Callback that puts up a force reply to the player prompting for how many points 
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                                                and call.data[0:28] == \
+                                                "HousePoints_award_points_to_")
+def callack_ask_how_many_points(call):
+    key = call.message.chat.id 
+    output = schools[key].how_many_points(call.from_user.id,
+                                          call.data[28:])
+    if output:
+        bot.edit_message_text(call.data[28:], 
+                              message_id=call.message.message_id, 
+                              chat_id=call.message.chat.id,
+                              reply_markup=None)
+        sent_message = bot.send_message(key, output[0], reply_markup= output[1])
+        messages_awaiting_responses[sent_message.message_id] = \
+                                                    call.from_user.id
+
+#Parses the force replay to add points to a house
+@bot.message_handler(func=lambda message: message.chat.id in schools
+                                      and message.reply_to_message != None)
+def award_points_to_house(message):
+    key = message.chat.id 
+    if message.from_user.id \
+    == messages_awaiting_responses[message.reply_to_message.message_id]:
+      output = schools[key].add_points(message.from_user.id, message.text)
+      del messages_awaiting_responses[message.reply_to_message.message_id]
+      pickle.dump( schools, open( "HousePoints_bot_Data.p", "wb" ) )
+      bot.reply_to(message, output[0], reply_markup=output[1])
+
+#Shows all the houses in a school and their points 
 @bot.message_handler(commands=['house_totals'])
-def PointsCount(message):
-    bot.send_message(message.chat.id, "Gryffindor:  " + str(HousePoints[0]) + 
-														 "\nSlytherin:  " + str(HousePoints[1]) + 
-														 "\nRavenclaw:  " + str(HousePoints[2]) + 
-														 "\nHufflepuff:  " + str(HousePoints[3]))
+def command_house_totals(message):
+    key = message.chat.id 
+    if key in schools:
+        output = schools[key].house_totals()
+        bot.reply_to(message, output)
+    else:
+        bot.reply_to(message, "There is not school in the chat.")
 
+#Shows a schools info, like who is the headmaster and who the prefects are
+@bot.message_handler(commands=['school_info'])
+def command_school_info(message): 
+    key = message.chat.id 
+    if key in schools:
+        output = schools[key].school_info()
+        bot.reply_to(message, output)
+    else:
+        bot.reply_to(message, "There is not school in the chat.")
+"""
+#Command that can be run by headmaster to change settings of the school
+@bot.message_handler(commands=['school_settings'])
+def command_school_settings(message):
+    key = message.chat.id
+    if key in schools:   
+        output = schools[key].school_settings(message.from_user.id)
+        if output:
+            try:
+                bot.edit_message_text(output[0], 
+                                      message_id=call.message.message_id, 
+                                      chat_id=call.message.chat.id,
+                                      reply_markup=output[1])
+            except: pass
+    else:
+        bot.reply_to(message, "There is not school in the chat.")
 
-#Command for anyone to use to see last months point scores of each house
-@bot.message_handler(commands=['last_month'])
-def PointsCount(message):
-    bot.send_message(message.chat.id, "Last months scores were \nGryffindor:  " + str(LastMonthsPoints[0]) + 
-																							  "\nSlytherin:  " + str(LastMonthsPoints[1]) + 
-																							  "\nRavenclaw:  " + str(LastMonthsPoints[2]) + 
-																							  "\nHufflepuff:  " + str(LastMonthsPoints[3]))
-	
-	
-#so names of houses can be called by their index
-def HouseNames(houseIdex):
-	if houseIdex == 0:
-		return "Gryffindor"
-	if houseIdex == 1:
-		return "Slytherin"
-	if houseIdex == 2:
-		return "Ravenclaw"
-	if houseIdex == 3:
-		return "Hufflepuff"
-	
-	
-#is numbers 2 becouse it is called from 1G, 1S, 1R, 1H
-def Addpoints2(index, message):
-	howMany = types.ForceReply(selective=True)
-	if message.from_user.id in prefects:
-		bot.reply_to(message, "How many points do you wish to award to " + str(HouseNames(index)) + "?", reply_markup=howMany)
-		userStep[message.from_user.id] = index
-	else:
-		bot.send_message(message.chat.id, "Only prefects can award points")
-	
-	
-#the four main comands of the bot
-@bot.message_handler(commands=['points_gryffindor'])
-def AddPoints1G(message):
-	Addpoints2(0, message)
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                              and call.data == "HousePoints_point_reset")
+def callback_school_settings_reset(call):
+    key = message.chat.id
+    output = schools[key].school_settings_reset(message.from_user.id)
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
 
-@bot.message_handler(commands=['points_slytherin'])
-def AddPoints1S(message):
-	Addpoints2(1, message)
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                            and call.data[0:24] == "HousePoints_point_reset_")
+def callback_school_settings_reset_options(call):
+    key = message.chat.id
+    output = schools[key].school_settings_reset(message.from_user.id, 
+                                                call.data[24:])
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
 
-@bot.message_handler(commands=['points_ravenclaw'])
-def AddPoints1R(message):
-	Addpoints2(2, message)
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                              and call.data == "HousePoints_remove_prefect")
+def callback_school_settings_remove_prefect(call):
+    key = message.chat.id
+    output = schools[key].school_settings_remove_prefect(message.from_user.id)
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
 
-@bot.message_handler(commands=['points_hufflepuff'])
-def AddPoints1H(message):
-	Addpoints2(3, message)
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                           and call.data[0:27] == "HousePoints_remove_prefect_")
+def callback_school_settings_remove_prefect_id(call):
+    key = message.chat.id
+    output = schools[key].school_settings_remove_prefect(message.from_user.id, 
+                                                         call.data[27:])
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
 
-	
-#this handles the responce to the four comands
-@bot.message_handler(func=lambda message: message.from_user.id in prefects and userStep[message.from_user.id] == 0)
-@bot.message_handler(func=lambda message: message.from_user.id in prefects and userStep[message.from_user.id] == 1)
-@bot.message_handler(func=lambda message: message.from_user.id in prefects and userStep[message.from_user.id] == 2)
-@bot.message_handler(func=lambda message: message.from_user.id in prefects and userStep[message.from_user.id] == 3)
-def AddPoints3(message):
-	try:
-		index = userStep[message.from_user.id]
-		HousePoints[index]+= int(message.text)
-		
-		with open("HousePoints_bot_record.pickle","wb") as newRecord:
-			pickle.dump(HousePoints, newRecord)
-		newRecord.close()
-		
-		bot.send_message(message.chat.id, message.text + " Points awarded to " + str(HouseNames(index)))
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                              and call.data == "HousePoints_remove_house")
+def callback_school_settings_remove_house(call):
+    key = message.chat.id
+    output = schools[key].school_settings_remove_house(message.from_user.id)
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
 
-	except:
-		bot.send_message(message.chat.id, "Sorry that is not valid input")
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                    and call.data[0:25] == "HousePoints_remove_house_")
+def callback_school_settings_remove_house_id(call):
+    key = message.chat.id
+    output = schools[key].school_settings_remove_house(message.from_user.id, 
+                                                         call.data[25:])
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
 
-	userStep[message.from_user.id] = 4
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                                    and call.data == "HousePoints_add_house")
+def callback_school_settings_add_house(call):
+    key = message.chat.id
+    output = schools[key].school_settings_new_house_name(message.from_user.id)
+    if output:
+        messages_awaiting_responses[call.message.message_id] = \
+                                                    call.message.from_user.id
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
 
-	
+#
+@bot.message_handler(func=lambda message: message.chat.id in schools
+                                      and message.reply_to_message.message_id \
+                                        in messages_awaiting_responses)
+def add_new_house_to_school():
+    key = message.chat.id 
+    if message.from_user.id \
+    == messages_awaiting_responses[message.reply_to_message.message_id]:
+      output = schools[key].add_house(message.from_user.id, message.text)
+      del users_adding_points[message.reply_to_message.message_id]
+      bot.reply_to(message, output[0], reply_markup=output[1])
 
-while True:
-	try:
-		bot.set_update_listener(LastMonthUpdate)
-		bot.polling()
-	except:
-		time.sleep(30)
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                                    and call.data == "HousePoints_close_school")
+def callback_school_settings_remove_house(call):
+    key = message.chat.id
+    output = schools[key].school_settings_close_school(message.from_user.id)
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
+
+#
+@bot.callback_query_handler(func=lambda call: call.message.chat.id in schools 
+                               and call.data == "HousePoints_close_school_sure")
+def callback_school_settings_remove_house_id(call):
+    key = message.chat.id
+    output = schools[key].school_settings_remove_house(message.from_user.id)
+    if output:
+        try:
+            bot.edit_message_text(output[0], 
+                                  message_id=call.message.message_id, 
+                                  chat_id=call.message.chat.id,
+                                  reply_markup=output[1])
+        except: pass
+
+#write a function to reset points and the end of the month/week
+"""
+bot.polling()
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # HousePoints_bot
-A telegram bot made to keep track of point eared by four teams and any user being able to call a function to check totals. Only certain users having the ability award points, these user are defined in the cfg file before the bot is started. 
+A telegram bot made to keep track of point eared by four teams and any user being able to call a function to check totals. Only certain users having the ability award points.
 
-Written in python 3.4
-
-# Dependencies
-eternnoir/pyTelegramBotAPI
+# Requires to run:    
+Python 2.7   
+[pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI)   
